0a81e14b3e70003223cb7eb6a38980df6ea58d32
cengiztuglu <cngztuglu@gmail.com>
2023-06-15 04:43:11 +0300

    Histogram Pictured Save

--- Histogram.py
+++ Histogram.py
@@ -1,6 +1,9 @@
+import os
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import euclidean
+
 
 def preprocess_audio(audio_file):
     audio_data, sample_rate = librosa.load(audio_file)
@@ -11,14 +14,34 @@ def preprocess_audio(audio_file):
     energy = np.sum(spectrogram, axis=0)
     return energy
 
-def create_histogram(energy):
+
+def create_histogram(energy, person_name, output_directory, index):
     histogram, bins = np.histogram(energy, bins=10, range=(0, np.max(energy)))
     plt.bar(bins[:-1], histogram, width=np.diff(bins))
     plt.xlabel("Enerji Aralığı")
     plt.ylabel("Frekans")
     plt.title("Enerji Histogramı")
-    plt.show()
+    output_path = os.path.join(output_directory, f"{person_name}_histogram_{index}.png")
+    plt.savefig(output_path)
+    plt.close()
 
-audio_file = "C:/Users/Cengiz/Desktop/Sesler/Actor_01/03-01-01-01-01-01-01.wav"
-energy = preprocess_audio(audio_file)
-create_histogram(energy)
+
+data_directory = "C:/Users/Cengiz/Desktop/Sesler"  # Veri seti dizini
+output_directory = "C:/Users/Cengiz/Desktop/histogram"  # Histogramların kaydedileceği ana dizin
+
+person_names = os.listdir(data_directory)  # Tüm kişi isimlerini al
+
+for person_name in person_names:
+    person_directory = os.path.join(data_directory, person_name)
+    output_person_directory = os.path.join(output_directory, person_name)
+
+    if os.path.isdir(person_directory):
+        os.makedirs(output_person_directory, exist_ok=True)  # Kişi için histogram klasörünü oluştur
+
+        file_names = os.listdir(person_directory)
+        file_names.sort()  # Dosya adlarını sırala
+
+        for index, file_name in enumerate(file_names):
+            file_path = os.path.join(person_directory, file_name)
+            energy = preprocess_audio(file_path)
+            create_histogram(energy, person_name, output_person_directory, index + 1)
